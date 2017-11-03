ee9610ed0a785fca13398c3046ea79cd806da14e
linfuxiang <linfuxiang@yaochufa.com>
2017-11-03 17:23:24 +0800

    添加说明

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 /**
  * 配置项
  * @key {Array} SPIDER_URL 网址列表，下面的是我自己添加的，不用的整行删除，需要的按照一样的格式添加一行就行了，记得前后加上单引号''
- * @key {String} DIRECTORY_NAME 保存爬虫结果的文件夹名字
+ * @key {String} DIRECTORY_NAME 保存爬虫结果的文件夹名字，没有的话会自动创建
  * @key {String} KEY_WORDS 关键字
  * @key {Number} KEY_WORDS_TIME 关键字出现次数，关键字出现超过这个次数才会被记录下来
  * @key {Number} ARTICLE_LENGTH 每篇文章长度，超过这个长度才会被记录下来
